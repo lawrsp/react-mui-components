a24e61bbfe867d7a32952de33cf2f0f441f57be9
lawrsp <7957003+lawrsp@users.noreply.github.com>
2022-11-13 14:23:34 +0800

    fix build and storybook

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,5 +1,5 @@
 import typescript from '@rollup/plugin-typescript';
-import pkg from './package.json' assert { type: 'json' };
+import pkg from './package.json' assert { type: 'json' }
 
 const EXTERNALS = Object.keys(pkg.dependencies)
   .concat(Object.keys(pkg.peerDependencies))
