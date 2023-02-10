b83ac75b56449f5b4023c111bb8a55712d826448
lawrsp <7957003+lawrsp@users.noreply.github.com>
2023-02-10 14:55:24 +0800

    export useProTableSearchProps

--- src/ProTable/index.tsx
+++ src/ProTable/index.tsx
@@ -1,5 +1,6 @@
 export * from './types';
 export * from './ProTable';
 export * from './SearchForm';
+export * from './useSearch';
 export * from './usePagination';
 export { default } from './ProTable';
