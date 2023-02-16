027631ebf2e4fa8a1f01365e44767bf0aa8b5d4a
lawrsp <7957003+lawrsp@users.noreply.github.com>
2023-02-16 14:55:39 +0800

    bump version

--- src/ProTable/index.tsx
+++ src/ProTable/index.tsx
@@ -1,6 +1,8 @@
 export * from './types';
 export * from './ProTable';
 export * from './SearchForm';
+export * from './useSearch';
+export * from './useSearchTitle';
 export * from './useSearchTool';
 export * from './usePagination';
 export { default } from './ProTable';
