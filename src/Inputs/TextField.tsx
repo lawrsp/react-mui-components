37c732f80c2d4975328bddc5f4beaf8fef4402fc
lawrsp <7957003+lawrsp@users.noreply.github.com>
2023-02-26 20:52:21 +0800

    fix TextField trim

--- src/Inputs/TextField.tsx
+++ src/Inputs/TextField.tsx
@@ -8,7 +8,7 @@ export type TextFieldProps = {
 } & Omit<MuiTextFieldProps, 'readOnly' | 'onChange'>;
 
 export const TextField = (props: TextFieldProps) => {
-  const { readOnly, InputProps, onChange, select, noTrim, ...rest } = props;
+  const { readOnly, InputProps, onChange, select, noTrim, onBlur, ...rest } = props;
   const reInputProps: TextFieldProps['InputProps'] = {
     ...InputProps,
     readOnly,
@@ -22,21 +22,42 @@ export const TextField = (props: TextFieldProps) => {
       return undefined;
     }
 
-    return async (ev: React.SyntheticEvent, value?: unknown) => {
+    return async (ev: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+      const value = ev.target.value;
       if (select) {
-        return await onChange(ev, (ev.target as any).value);
+        return await onChange(ev, value);
       }
 
       if (!noTrim && typeof value === 'string') {
-        return await onChange(ev, value.trim);
+        return await onChange(ev, value.trimStart());
       }
 
       return await onChange(ev, value);
     };
-  }, [onChange, select]);
+  }, [onChange, select, !noTrim]);
+
+  const handleOnBlur = (ev: React.FocusEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const value = ev.target.value;
+    if (!noTrim && typeof value === 'string') {
+      ev.target.value = value.trim();
+      if (onChange) {
+        onChange(ev, ev.target.value);
+      }
+    }
+
+    if (onBlur) {
+      onBlur(ev);
+    }
+  };
 
   return (
-    <MuiTextField InputProps={reInputProps} onChange={handleChange} select={isSelect} {...rest} />
+    <MuiTextField
+      InputProps={reInputProps}
+      onChange={handleChange}
+      select={isSelect}
+      onBlur={handleOnBlur}
+      {...rest}
+    />
   );
 };
 
